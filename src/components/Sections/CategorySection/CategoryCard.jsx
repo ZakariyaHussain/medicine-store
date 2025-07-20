56abd1c5dff8b08eedd597c0c26ac19cb3fbee15
Zakariya Hussian <zakariyahussain1989@gmail.com>
2025-07-20 17:51:32 +0600

    category card ok

--- src/components/Sections/CategorySection/CategoryCard.jsx
+++ src/components/Sections/CategorySection/CategoryCard.jsx
@@ -3,25 +3,26 @@ import { Link } from 'react-router';
 
 
 
-const CategoryCard = ({ medicine }) => {
-    const { category, image } = medicine || {};
-    console.log(medicine);
+const CategoryCard = ({ category, image, count }) => {
+    //const { category, image} = medicine || {};
+    console.log(count);
+    //console.log(count);
 
 
 
     return (
         <Link to={`/category/${category}`}>
             <div className="card bg-base-100 shadow-xl">
-                <figure>
+                <figure className='max-h-[250px]'>
                     <img
                         src={image}
                         alt="Shoes" />
                 </figure>
                 <div className="card-body">
                     <p className="text-lg font-bold">Category Name: <span className='font-normal'>{category}</span></p>
-                    <p className='text-lg font-bold'>Medicines Number (In this Category): <span className='font-normal'>{category}</span></p>
+                    <p className='text-lg font-bold'>Medicines Number (In this Category): <span className='font-normal'>{count}</span></p>
                     <div className="card-actions">
-                        <button className="btn bg-green-400 text-white hover:touch-pinch-zoom">Book Now</button>
+                        <button className="btn bg-green-400 text-white hover:touch-pinch-zoom">Show All Items In This Category</button>
                     </div>
                 </div>
             </div>
--- src/components/Sections/CategorySection/CategorySection.jsx
+++ src/components/Sections/CategorySection/CategorySection.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router';
 import CategoryCard from './CategoryCard';
+import axios from 'axios';
 
 const CategorySection = () => {
     const medicines = useLoaderData();
     console.log(medicines);
+    const [categories, setCategories] = useState([]);
+    console.log(categories);
+    useEffect(() => {
+        axios.get('http://localhost:5000/category-stats')
+            .then(res => setCategories(res.data))
+            .catch(err => console.error(err));
+    }, []);
     return (
         <div className='px-6 mx-auto'>
-                <div className='mt-16 mb-10'>
-                    <h2 className='text-2xl font-bold text-center'>All Medicines By Category</h2>
-                </div>
-                {/* Cards */}
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-                    {
-                        medicines.map(medicine => <CategoryCard key={medicine._id} medicine={medicine}></CategoryCard>)
-                    }
-                </div>
+            <div className='mt-16 mb-10'>
+                <h2 className='text-2xl font-bold text-center'>All Medicines By Category</h2>
             </div>
+            {/* Cards */}
+            {/* <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {
+                    medicines.map(medicine => <CategoryCard key={medicine._id} medicine={medicine} categories={categories}></CategoryCard>)
+                }
+            </div> */}
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+                {categories.map(cat => (
+                    <CategoryCard
+                        key={cat.category}
+                        category={cat.category}
+                        image={cat.image}
+                        count={cat.count}
+                    />
+                ))}
+            </div>
+        </div>
     );
 };
 
